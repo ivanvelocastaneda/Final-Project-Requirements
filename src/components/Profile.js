2f093c173d478828cb30e7ca1513df43d5abdef6
Ivan Velo Castaneda <111911650+ivanvelocastaneda@users.noreply.github.com>
2023-05-09 20:00:44 -0500

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,4 @@
+// This component displays the profile of the user and their information 
 import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import { auth, db } from "../config/firebaseConfig";
