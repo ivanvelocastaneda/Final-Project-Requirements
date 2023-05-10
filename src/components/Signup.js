fd9d0afbdfad930f2e6c9776f1b989bef6dd59fb
Ivan Velo Castaneda <111911650+ivanvelocastaneda@users.noreply.github.com>
2023-05-09 20:01:29 -0500

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,3 +1,4 @@
+// This component allows the user to sign up and create an account
 import React, { useState } from "react";
 import NavBar from "./NavBar";
 import { useNavigate } from "react-router-dom";
